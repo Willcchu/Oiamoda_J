c96a4d743ee3b228924b0b748b5170b8b0ed9748
Chun Chu <williamchu0605@gmail.com>
2024-10-16 22:54:45 -0300

    fixed the bugs about the favoritesView, and some in app page, still not completed though

--- Oiamoda J/MainViews/Oiamoda_JApp.swift
+++ Oiamoda J/MainViews/Oiamoda_JApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct Oiamoda_JApp: App {
+    @StateObject private var favoritesManager = FavoritesManager()
+    
+    
     var body: some Scene {
+        
+        
         WindowGroup {
             SplashScreenView()
+                .environmentObject(FavoritesManager())
+
         }
     }
 }
+
+
