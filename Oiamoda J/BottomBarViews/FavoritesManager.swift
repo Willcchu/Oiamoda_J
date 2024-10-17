8f7bf0b3286980b7b2c4689ac6430a3fa8546f1a
Chun Chu <williamchu0605@gmail.com>
2024-10-16 22:18:17 -0300

    added a Favorite function, not completed

---
+++ Oiamoda J/BottomBarViews/FavoritesManager.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesManager.swift
+//  Oiamoda J
+//
+//  Created by CHUN CHU on 2024-10-16.
+//
+
+import SwiftUI
+
+struct FavoritesManager: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesManager()
+}
---
+++ Oiamoda J/BottomBarViews/FavoritesView.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesView.swift
+//  Oiamoda J
+//
+//  Created by CHUN CHU on 2024-10-16.
+//
+
+import SwiftUI
+
+struct FavoritesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoritesView()
+}
